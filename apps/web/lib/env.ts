e6e6721bf91886091066001e798f2d7dbbeed8ca
Stefan Bisti <stefbisti@gmail.com>
2026-08-24 17:44:54 +0300

    feat(web): add zod validation of env vars
    Signed-off-by: Stefan Bisti <stefbisti@gmail.com>

---
+++ apps/web/lib/env.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+const schema = z.object({
+  DATABASE_URL: z.url(),
+  AUTH_SECRET: z.string().min(1),
+  AUTH_GITHUB_ID: z.string().min(1),
+  AUTH_GITHUB_SECRET: z.string().min(1),
+  NEXT_PUBLIC_APP_URL: z.url(),
+});
+
+export const env = schema.parse(process.env);
